98500a4763ce28e8ca5d5ca0fe998b6e3bcccaeb
alirezasadeghi88 <alirezasadeghi0558@gmail.com>
2026-08-24 06:42:29 +0330

    Add class ComputeAverage

---
+++ ch2/src/ComputeAverage.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class ComputeAverage {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter three numbers: ");
+        double number1 = scanner.nextDouble();
+        double number2 = scanner.nextDouble();
+        double number3 = scanner.nextDouble();
+
+        double average = (number1 + number2 + number3) / 3;
+        System.out.println("The average of " + number1 + " " + number2
+                + " " + number3 + " is " + average);
+    }
+}
