69dd488f77c9134537adcbc8e9a7d8f6111c7524
alirezasadeghi88 <alirezasadeghi0558@gmail.com>
2026-08-26 06:51:59 +0330

    Add class AdditionQuiz

--- ch3/src/AdditionQuiz.java
+++ ch3/src/AdditionQuiz.java
@@ -1,8 +1,10 @@
+import java.util.Scanner;
+
 public class AdditionQuiz {
     public static void main(String[] args) {
         int number1 = (int)(System.currentTimeMillis() % 10);
         int number2 = (int)(System.currentTimeMillis() / 10 % 10);
 
-
+        Scanner input = new Scanner(System.in);
     }
 }
