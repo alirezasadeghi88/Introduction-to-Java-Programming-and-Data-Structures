c8d727e7ae7d8742e99f48b0c47c8147f9f6b301
alirezasadeghi88 <alirezasadeghi0558@gmail.com>
2026-08-26 06:51:34 +0330

    Add class AdditionQuiz

---
+++ ch3/src/AdditionQuiz.java
@@ -0,0 +1,8 @@
+public class AdditionQuiz {
+    public static void main(String[] args) {
+        int number1 = (int)(System.currentTimeMillis() % 10);
+        int number2 = (int)(System.currentTimeMillis() / 10 % 10);
+
+
+    }
+}
