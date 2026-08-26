3cc61e1a4bda0c554014c43801a5573d57ad7867
alirezasadeghi88 <alirezasadeghi0558@gmail.com>
2026-08-26 06:58:28 +0330

    Add class SubtractionQuiz

--- ch3/src/SubtractionQuiz.java
+++ ch3/src/SubtractionQuiz.java
@@ -1,2 +1,6 @@
 public class SubtractionQuiz {
+    public static void main(String[] args) {
+        int number1 = (int)(Math.random() * 10);
+        int number2 = (int)(Math.random() * 10);
+    }
 }
