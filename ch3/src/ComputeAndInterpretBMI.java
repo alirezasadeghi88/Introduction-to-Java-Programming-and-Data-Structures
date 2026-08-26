c665bd2e74b347a043b841f803b08c7506b3e85d
alirezasadeghi88 <alirezasadeghi0558@gmail.com>
2026-08-26 07:08:37 +0330

    Add class ComputeAndInterpretBMI

--- ch3/src/ComputeAndInterpretBMI.java
+++ ch3/src/ComputeAndInterpretBMI.java
@@ -3,5 +3,8 @@ import java.util.Scanner;
 public class ComputeAndInterpretBMI {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter weight in pounds: ");
+        double weight = input.nextDouble();
     }
 }
