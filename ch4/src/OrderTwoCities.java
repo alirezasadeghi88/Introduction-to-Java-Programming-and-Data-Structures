6e4e5565414a9a7b478c43a589f1f4012a938cf6
alirezasadeghi88 <alirezasadeghi0558@gmail.com>
2026-08-28 07:11:32 +0330

    Add class OrderTwoCities

--- ch4/src/OrderTwoCities.java
+++ ch4/src/OrderTwoCities.java
@@ -3,5 +3,8 @@ import java.util.Scanner;
 public class OrderTwoCities {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter the first city: ");
+        String city1 = input.nextLine();
     }
 }
