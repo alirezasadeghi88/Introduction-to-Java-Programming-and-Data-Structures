9147ed39e5eb3be0626916b1d7772aea0d6d0cd8
alirezasadeghi88 <alirezasadeghi0558@gmail.com>
2026-08-28 07:17:26 +0330

    Add class LotteryUsingStrings

--- ch4/src/LotteryUsingStrings.java
+++ ch4/src/LotteryUsingStrings.java
@@ -1,6 +1,10 @@
+import java.util.Scanner;
+
 public class LotteryUsingStrings {
     public static void main(String[] args) {
         String lottery = "" + (int)(Math.random() * 10)
                + (int)(Math.random() * 10);
+
+        Scanner input = new Scanner(System.in);
     }
 }
