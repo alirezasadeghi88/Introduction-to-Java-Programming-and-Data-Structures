cdd12159a67adb912234a1c10c5fede3f33ab483
alirezasadeghi88 <alirezasadeghi0558@gmail.com>
2026-08-28 07:03:30 +0330

    Add class ComputeAngles

--- ch4/src/ComputeAngles.java
+++ ch4/src/ComputeAngles.java
@@ -16,6 +16,7 @@ public class ComputeAngles {
             + (y2 - y3) * (y2 - y3));
         double b = Math.sqrt((x1 - x3) * (x1 - x3)
             + (y1 - y3) * (y1 - y3));
-
+        double c = Math.sqrt((x1 - x2) * (x1 - x2)
+                + (y1 - y2) * (y1 - y2));
     }
 }
