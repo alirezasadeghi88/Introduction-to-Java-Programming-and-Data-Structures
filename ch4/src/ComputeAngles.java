c6c1ee1b0de86244699cf9c2a81ef44c16d5d1a4
alirezasadeghi88 <alirezasadeghi0558@gmail.com>
2026-08-28 07:00:22 +0330

    Add class ComputeAngles

--- ch4/src/ComputeAngles.java
+++ ch4/src/ComputeAngles.java
@@ -3,5 +3,9 @@ import java.util.Scanner;
 public class ComputeAngles {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter three points: ");
+        double x1 = input.nextDouble();
+        double y1 = input.nextDouble();
     }
 }
