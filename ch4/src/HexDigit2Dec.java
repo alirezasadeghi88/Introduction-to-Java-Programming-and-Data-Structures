d72faedc23706b10fc35c59baa97beb701437eee
alirezasadeghi88 <alirezasadeghi0558@gmail.com>
2026-08-28 07:14:13 +0330

    Add class HexDigit2Dec

--- ch4/src/HexDigit2Dec.java
+++ ch4/src/HexDigit2Dec.java
@@ -3,5 +3,8 @@ import java.util.Scanner;
 public class HexDigit2Dec {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter a hex digit: ");
+        String hexString = input.nextLine();
     }
 }
