ed0c4fc6b09c1cf2480e30e71390c40664244425
alirezasadeghi88 <alirezasadeghi0558@gmail.com>
2026-08-28 07:17:05 +0330

    Add class LotteryUsingStrings

---
+++ ch4/src/LotteryUsingStrings.java
@@ -0,0 +1,6 @@
+public class LotteryUsingStrings {
+    public static void main(String[] args) {
+        String lottery = "" + (int)(Math.random() * 10)
+               + (int)(Math.random() * 10);
+    }
+}
