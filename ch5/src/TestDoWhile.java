02e5402548d2d264343658ef50f41b2f1fdba478
alirezasadeghi88 <alirezasadeghi0558@gmail.com>
2026-08-30 07:02:09 +0330

    Add class TestDoWhile

--- ch5/src/TestDoWhile.java
+++ ch5/src/TestDoWhile.java
@@ -1,6 +1,11 @@
+import java.util.Scanner;
+
 public class TestDoWhile {
     public static void main(String[] args) {
         int data;
         int sum = 0;
+
+        Scanner input = new Scanner(System.in);
+
     }
 }
