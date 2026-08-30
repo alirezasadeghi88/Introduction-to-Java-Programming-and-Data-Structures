6b0e8a1fbc9a9a0cbe8a2b1a9fd494def89458a3
alirezasadeghi88 <alirezasadeghi0558@gmail.com>
2026-08-30 06:53:26 +0330

    Add class GuessNumber

--- ch5/src/GuessNumber.java
+++ ch5/src/GuessNumber.java
@@ -8,6 +8,15 @@ public class GuessNumber {
         System.out.println("Guess a magic number between 0 and 100");
 
         int guess = -1;
-
+        while (guess != number) {
+            System.out.print("\nEnter your guess: ");
+            guess = input.nextInt();
+            if (guess == number)
+                System.out.println("Yes, the number is " + number);
+            else if (guess > number)
+                System.out.println("Your guess is too high");
+            else
+                System.out.println("Your guess is too low");
+        }
     }
 }
