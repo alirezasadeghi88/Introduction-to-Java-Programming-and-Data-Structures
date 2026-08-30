e6f0af51794d59ab9a87848a14e7714db08bd211
alirezasadeghi88 <alirezasadeghi0558@gmail.com>
2026-08-30 06:43:19 +0330

    Add class GuessNumberOneTime

--- ch5/src/GuessNumberOneTime.java
+++ ch5/src/GuessNumberOneTime.java
@@ -1,5 +1,9 @@
+import java.util.Scanner;
+
 public class GuessNumberOneTime {
     public static void main(String[] args) {
         int number = (int)(Math.random() * 101);
+
+        Scanner input = new Scanner(System.in);
     }
 }
