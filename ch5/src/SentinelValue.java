ba5e0a60a1a025136eae025babebdee034aaff1f
alirezasadeghi88 <alirezasadeghi0558@gmail.com>
2026-08-30 06:56:35 +0330

    Add class SentinelValue

--- ch5/src/SentinelValue.java
+++ ch5/src/SentinelValue.java
@@ -3,5 +3,9 @@ import java.util.Scanner;
 public class SentinelValue {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+
+        System.out.print(
+                "Enter an integer (the input ends if it is 0): ");
+        int data = input.nextInt();
     }
 }
