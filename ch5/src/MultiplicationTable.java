5b47b037b50d2252f689cf8a37a6dc037186373e
alirezasadeghi88 <alirezasadeghi0558@gmail.com>
2026-08-30 07:06:38 +0330

    Add class MultiplicationTable

--- ch5/src/MultiplicationTable.java
+++ ch5/src/MultiplicationTable.java
@@ -5,5 +5,7 @@ public class MultiplicationTable {
         System.out.print("   ");
         for (int j = 1; j <= 9; j++)
             System.out.print("   " + j);
+
+        System.out.println("\n — — — — — — — — — — —— — — — — — — — —");
     }
 }
