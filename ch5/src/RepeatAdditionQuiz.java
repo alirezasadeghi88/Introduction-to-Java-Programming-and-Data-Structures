3d192a8c7ea101c9d4cc10b8f942df42b0b07515
alirezasadeghi88 <alirezasadeghi0558@gmail.com>
2026-08-30 06:39:14 +0330

    Add class RepeatAdditionQuiz

--- ch5/src/RepeatAdditionQuiz.java
+++ ch5/src/RepeatAdditionQuiz.java
@@ -1,6 +1,10 @@
+import java.util.Scanner;
+
 public class RepeatAdditionQuiz {
     public static void main(String[] args) {
         int number1 = (int)(Math.random() * 10);
         int number2 = (int)(Math.random() * 10);
+
+        Scanner input = new Scanner(System.in);
     }
 }
