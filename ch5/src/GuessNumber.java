5fa68f60dfc116cbe2ea2263f435aea832ae88b3
alirezasadeghi88 <alirezasadeghi0558@gmail.com>
2026-08-30 06:51:21 +0330

    Add class GuessNumber

--- ch5/src/GuessNumber.java
+++ ch5/src/GuessNumber.java
@@ -1,5 +1,10 @@
+import java.util.Scanner;
+
 public class GuessNumber {
     public static void main(String[] args) {
         int number = (int)(Math.random() * 101);
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("Guess a magic number between 0 and 100");
     }
 }
