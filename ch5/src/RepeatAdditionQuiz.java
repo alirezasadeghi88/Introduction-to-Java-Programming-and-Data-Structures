643c82f39677b50db787079faebe475cf3eb787f
alirezasadeghi88 <alirezasadeghi0558@gmail.com>
2026-08-30 06:38:49 +0330

    Add class RepeatAdditionQuiz

--- ch5/src/RepeatAdditionQuiz.java
+++ ch5/src/RepeatAdditionQuiz.java
@@ -1,5 +1,6 @@
 public class RepeatAdditionQuiz {
     public static void main(String[] args) {
-
+        int number1 = (int)(Math.random() * 10);
+        int number2 = (int)(Math.random() * 10);
     }
 }
