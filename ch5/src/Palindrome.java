c84c17f118aaa65cc707bea7838e344620a462d6
alirezasadeghi88 <alirezasadeghi0558@gmail.com>
2026-08-31 06:57:49 +0330

    Add class Palindrome

--- ch5/src/Palindrome.java
+++ ch5/src/Palindrome.java
@@ -10,5 +10,14 @@ public class Palindrome {
         int low = 0;
         int high = s.length() - 1;
         boolean isPalindrome = true;
+
+        while (low < high) {
+            if (s.charAt(low) != s.charAt(high)) {
+                isPalindrome = false;
+                break;
+            }
+            low++;
+            high--;
+        }
     }
 }
