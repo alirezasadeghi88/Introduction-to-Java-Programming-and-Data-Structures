be4d144ca615f4a301cdc706f29a4cf24df3b1fd
alirezasadeghi88 <alirezasadeghi0558@gmail.com>
2026-08-31 06:39:32 +0330

    Add class Dec2Hex

--- ch5/src/Dec2Hex.java
+++ ch5/src/Dec2Hex.java
@@ -8,5 +8,15 @@ public class Dec2Hex {
         int decimal = input.nextInt();
 
         String hex = "";
+
+        while (decimal != 0) {
+            int hexValue = decimal % 16;
+
+            char hexDigit = (0 <= hexValue && hexValue <= 9)?
+                    (char)(hexValue + '0'): (char)(hexValue - 10 + 'A');
+
+            hex = hexDigit + hex;
+            decimal = decimal / 16;
+        }
     }
 }
