a66fadb6f90d0740d5c644b70c676e2aa4ed7a85
alirezasadeghi88 <alirezasadeghi0558@gmail.com>
2026-08-31 06:56:18 +0330

    Add class Palindrome

--- ch5/src/Palindrome.java
+++ ch5/src/Palindrome.java
@@ -3,5 +3,8 @@ import java.util.Scanner;
 public class Palindrome {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter a string: ");
+        String s = input.nextLine();
     }
 }
