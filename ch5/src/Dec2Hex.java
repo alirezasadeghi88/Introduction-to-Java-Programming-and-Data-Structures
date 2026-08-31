96a028884041143af1e14cff844dfe118352f180
alirezasadeghi88 <alirezasadeghi0558@gmail.com>
2026-08-31 06:37:16 +0330

    Add class Dec2Hex

--- ch5/src/Dec2Hex.java
+++ ch5/src/Dec2Hex.java
@@ -3,5 +3,8 @@ import java.util.Scanner;
 public class Dec2Hex {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter a decimal number: ");
+        int decimal = input.nextInt();
     }
 }
