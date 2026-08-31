2e4c388ab0545cc0786c953c2d5fa0ef1af01b8c
alirezasadeghi88 <alirezasadeghi0558@gmail.com>
2026-08-31 06:45:16 +0330

    Add class TestBreak

--- ch5/src/TestBreak.java
+++ ch5/src/TestBreak.java
@@ -2,5 +2,12 @@ public class TestBreak {
     public static void main(String[] args) {
         int sum = 0;
         int number = 0;
+
+        while (number < 20) {
+            number++;
+            sum += number;
+            if (sum >= 100)
+                break;
+        }
     }
 }
