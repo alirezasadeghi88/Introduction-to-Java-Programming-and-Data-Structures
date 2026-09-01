1e23cd2bfb8922eefe0ee9d143b5af68267ac5fd
alirezasadeghi88 <alirezasadeghi0558@gmail.com>
2026-09-01 07:14:33 +0330

    Add class PrimeNumberMethod

--- ch6/src/PrimeNumberMethod.java
+++ ch6/src/PrimeNumberMethod.java
@@ -22,7 +22,11 @@ public class PrimeNumberMethod {
     }
 
     public static boolean isPrime(int number) {
-
+        for (int divisor = 2; divisor <= number / 2; divisor++) {
+            if (number % divisor == 0) {
+                return false;
+            }
+        }
     }
 }
 
