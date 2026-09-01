689687649885a2760965e3b42de69908541605bb
alirezasadeghi88 <alirezasadeghi0558@gmail.com>
2026-09-01 07:11:40 +0330

    Add class PrimeNumberMethod

--- ch6/src/PrimeNumberMethod.java
+++ ch6/src/PrimeNumberMethod.java
@@ -4,6 +4,8 @@ public class PrimeNumberMethod {
     }
 
     public static void printPrimeNumbers(int numberOfPrimes) {
-
+        final int NUMBER_OF_PRIMES_PER_LINE = 10;
+        int count = 0;
+        int number = 2;
     }
 }
