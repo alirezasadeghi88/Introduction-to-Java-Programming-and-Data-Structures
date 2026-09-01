1d2766cfc92c0b413561307fbba877e3c09157ac
alirezasadeghi88 <alirezasadeghi0558@gmail.com>
2026-09-01 07:05:05 +0330

    Add class GreatestCommonDivisorMethod

--- ch6/src/GreatestCommonDivisorMethod.java
+++ ch6/src/GreatestCommonDivisorMethod.java
@@ -6,5 +6,11 @@ public class GreatestCommonDivisorMethod {
     public static int gcd(int n1,int n2) {
         int gcd = 1;
         int k = 2;
+
+        while (k <= n1 && k <= n2) {
+            if (n1 % k == 0 && n2 % k == 0)
+                gcd = k;
+            k++;
+        }
     }
 }
