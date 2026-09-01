67c4f4bba33e379503f4e8f9f2a89660b8c9367b
alirezasadeghi88 <alirezasadeghi0558@gmail.com>
2026-09-01 06:53:11 +0330

    Add class Increment

--- ch6/src/Increment.java
+++ ch6/src/Increment.java
@@ -1,6 +1,9 @@
 public class Increment {
     public static void main(String[] args) {
         int x = 1;
+        System.out.println("Before the call, x is " + x);
+        increment(x);
+        System.out.println("After the call, x is " + x);
     }
 
 
