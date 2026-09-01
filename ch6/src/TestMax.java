515f061a150b96463c0b3dbb0064f200344d8c43
alirezasadeghi88 <alirezasadeghi0558@gmail.com>
2026-09-01 06:39:52 +0330

    Add class TestMax

--- ch6/src/TestMax.java
+++ ch6/src/TestMax.java
@@ -5,5 +5,11 @@ public class TestMax {
 
     public static int max(int num1, int num2) {
         int result;
+        if (num1 > num2)
+            result = num1;
+        else
+            result = num2;
+
+        return result;
     }
 }
