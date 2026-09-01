573fac622f86a400eb004addc838e15090916fbd
alirezasadeghi88 <alirezasadeghi0558@gmail.com>
2026-09-01 06:59:07 +0330

    Add class TestPassByValue

--- ch6/src/TestPassByValue.java
+++ ch6/src/TestPassByValue.java
@@ -4,6 +4,8 @@ public class TestPassByValue {
     }
 
     public static void swap(int n1, int n2) {
-
+        System.out.println("\tInside the swap method");
+        System.out.println("\t\tBefore swapping, n1 is " + n1
+                + " and n2 is " + n2);
     }
 }
