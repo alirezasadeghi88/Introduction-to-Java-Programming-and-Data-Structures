891d0372566c58cb772ef9314652fed0af00de81
alirezasadeghi88 <alirezasadeghi0558@gmail.com>
2026-09-01 07:06:15 +0330

    Add class GreatestCommonDivisorMethod

--- ch6/src/GreatestCommonDivisorMethod.java
+++ ch6/src/GreatestCommonDivisorMethod.java
@@ -1,6 +1,11 @@
+import java.util.Scanner;
+
 public class GreatestCommonDivisorMethod {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter first integer: ");
+        int n1 = input.nextInt();
     }
 
     public static int gcd(int n1,int n2) {
