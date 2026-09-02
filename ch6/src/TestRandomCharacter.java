5b68abe12373ad45c63c0815c9643780a4dc638b
alirezasadeghi88 <alirezasadeghi0558@gmail.com>
2026-09-02 07:11:53 +0330

    Add class RandomCharacter

--- ch6/src/TestRandomCharacter.java
+++ ch6/src/TestRandomCharacter.java
@@ -2,5 +2,9 @@ public class TestRandomCharacter {
     public static void main(String[] args) {
         final int NUMBER_OF_CHARS = 175;
         final int CHARS_PER_LINE = 25;
+
+        for (int i = 0; i < NUMBER_OF_CHARS; i++) {
+            char ch = RandomCharacter.getRandomLowerCaseLetter();
+        }
     }
 }
