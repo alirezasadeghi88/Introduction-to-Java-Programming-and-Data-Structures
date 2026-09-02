c01aaebea8e3c6cbaac26bbdaf7b58696cd846b8
alirezasadeghi88 <alirezasadeghi0558@gmail.com>
2026-09-02 07:01:54 +0330

    Add class TestMethodOverloading

--- ch6/src/TestMethodOverloading.java
+++ ch6/src/TestMethodOverloading.java
@@ -12,6 +12,8 @@ public class TestMethodOverloading {
 
     public static double max(double num1, double num2) {
         if (num1 > num2)
-        return num1;
+            return num1;
+        else
+            return num2;
     }
 }
