0446dabad810d92232cc3b48548f912324b4d486
alirezasadeghi88 <alirezasadeghi0558@gmail.com>
2026-09-02 06:51:04 +0330

    Add class Hex2Dec

--- ch6/src/Hex2Dec.java
+++ ch6/src/Hex2Dec.java
@@ -5,5 +5,10 @@ public class Hex2Dec {
 
     public static int hexToDecimal(String hex) {
         int decimalValue = 0;
+        for (int i = 0; i < hex.length(); i++) {
+             char hexChar = hex.charAt(i);
+             decimalValue = decimalValue * 16 + hexCharToDecimal(hexChar);
+        }
+
     }
 }
