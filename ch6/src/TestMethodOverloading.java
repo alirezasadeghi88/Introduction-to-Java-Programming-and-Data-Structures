4b742803a36b252bde9b9316f94e883e08e5f00d
alirezasadeghi88 <alirezasadeghi0558@gmail.com>
2026-09-02 07:03:31 +0330

    Add class TestMethodOverloading

--- ch6/src/TestMethodOverloading.java
+++ ch6/src/TestMethodOverloading.java
@@ -2,6 +2,9 @@ public class TestMethodOverloading {
     public static void main(String[] args) {
         System.out.println("The maximum of 3 and 4 is "
                 + max(3, 4));
+
+        System.out.println("The maximum of 3.0 and 5.4 is "
+                + max(3.0, 5.4));
     }
 
     public static int max(int num1, int num2) {
