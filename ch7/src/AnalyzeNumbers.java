757b62fceeb11a9d9692948520a89def61b6e6b4
alirezasadeghi88 <alirezasadeghi0558@gmail.com>
2026-09-03 06:46:54 +0330

    Add class AnalyzeNumbers

--- ch7/src/AnalyzeNumbers.java
+++ ch7/src/AnalyzeNumbers.java
@@ -1,5 +1,8 @@
 public class AnalyzeNumbers {
     public static void main(String[] args) {
         java.util.Scanner input = new java.util.Scanner(System.in);
+
+        System.out.print("Enter the number of items: ");
+        int n = input.nextInt();
     }
 }
