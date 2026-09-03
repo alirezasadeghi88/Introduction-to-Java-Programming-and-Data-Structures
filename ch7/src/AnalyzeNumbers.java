08a116f774783e97c324dd2ee4a704dd54bfcbbb
alirezasadeghi88 <alirezasadeghi0558@gmail.com>
2026-09-03 06:49:18 +0330

    Add class AnalyzeNumbers

--- ch7/src/AnalyzeNumbers.java
+++ ch7/src/AnalyzeNumbers.java
@@ -16,5 +16,8 @@ public class AnalyzeNumbers {
         double average = sum / n;
 
         int count = 0;
+        for (int i = 0; i < n; i++)
+            if (numbers[i] > average)
+                count++;
     }
 }
