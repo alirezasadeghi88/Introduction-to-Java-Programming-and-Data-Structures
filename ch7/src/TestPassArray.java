e0befcd2516a6ff448d112a041886e9a2b7ba4e9
alirezasadeghi88 <alirezasadeghi0558@gmail.com>
2026-09-03 07:07:01 +0330

    Add class TestPassArray

--- ch7/src/TestPassArray.java
+++ ch7/src/TestPassArray.java
@@ -16,6 +16,8 @@ public class TestPassArray {
     }
 
     public static void swapFirstTwoInArray(int[] array) {
-
+        int temp = array[0];
+        array[0] = array[1];
+        array[1] = temp;
     }
 }
