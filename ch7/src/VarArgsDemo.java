48c684adc6bbae6c0f3a17886c99fbcdc1ba9fa1
alirezasadeghi88 <alirezasadeghi0558@gmail.com>
2026-09-04 07:04:59 +0330

    Add class VarArgsDemo

--- ch7/src/VarArgsDemo.java
+++ ch7/src/VarArgsDemo.java
@@ -4,6 +4,9 @@ public class VarArgsDemo {
     }
 
     public static void printMax(double... numbers) {
-
+        if (numbers.length == 0) {
+            System.out.println("No argument passed");
+            return;
+        }
     }
 }
