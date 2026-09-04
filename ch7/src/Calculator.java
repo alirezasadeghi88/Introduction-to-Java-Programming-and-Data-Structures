b1d7445ca1ad7d5f9a7c98167d5686855d4e7185
alirezasadeghi88 <alirezasadeghi0558@gmail.com>
2026-09-04 07:10:18 +0330

    Add class Calculator

--- ch7/src/Calculator.java
+++ ch7/src/Calculator.java
@@ -1,5 +1,9 @@
 public class Calculator {
     public static void main(String[] args) {
-
+        if (args.length != 3) {
+            System.out.println(
+                    "Usage: java Calculator operand1 operator operand2");
+            System.exit(1);
+        }
     }
 }
