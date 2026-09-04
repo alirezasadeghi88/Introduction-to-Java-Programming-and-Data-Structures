488e9a60a0f81cd7e09a363b85ba278a42a325bc
alirezasadeghi88 <alirezasadeghi0558@gmail.com>
2026-09-04 06:51:17 +0330

    Add class CountLettersInArray

--- ch7/src/CountLettersInArray.java
+++ ch7/src/CountLettersInArray.java
@@ -5,5 +5,8 @@ public class CountLettersInArray {
 
     public static char[] createArray() {
         char[] chars = new char[100];
+
+        for (int i = 0; i < chars.length; i++)
+            chars[i] = RandomCharacter.getRandomLowerCaseLetter();
     }
 }
