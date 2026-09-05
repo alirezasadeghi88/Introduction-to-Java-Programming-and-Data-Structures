082a8beb0e0b72f11e4078fe08baf5e9eed60ce0
alirezasadeghi88 <alirezasadeghi0558@gmail.com>
2026-09-05 06:49:21 +0330

    Add class PassTwoDimensionalArray

--- ch8/src/PassTwoDimensionalArray.java
+++ ch8/src/PassTwoDimensionalArray.java
@@ -11,5 +11,8 @@ public class PassTwoDimensionalArray {
         int[][] m = new int[3][4];
         System.out.println("Enter " + m.length + " rows and "
                 + m[0].length + " columns: ");
+        for (int i = 0; i < m.length; i++)
+            for (int j = 0; j < m[i].length; j++)
+                m[i][j] = input.nextInt();
     }
 }
