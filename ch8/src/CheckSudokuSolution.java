0aa4be4723e24755561f0f42cddadaa097a2f424
alirezasadeghi88 <alirezasadeghi0558@gmail.com>
2026-09-05 06:56:36 +0330

    Add class CheckSudokuSolution

--- ch8/src/CheckSudokuSolution.java
+++ ch8/src/CheckSudokuSolution.java
@@ -8,5 +8,6 @@ public class CheckSudokuSolution {
 
     public static int[][] readASolution() {
         Scanner input = new Scanner(System.in);
+        System.out.println("Enter a Sudoku puzzle solution:");
     }
 }
