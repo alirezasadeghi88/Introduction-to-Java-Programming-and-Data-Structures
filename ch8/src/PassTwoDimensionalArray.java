4418b324449c3ae5927bf2850df3ac732a5d434d
alirezasadeghi88 <alirezasadeghi0558@gmail.com>
2026-09-05 06:47:49 +0330

    Add class PassTwoDimensionalArray

--- ch8/src/PassTwoDimensionalArray.java
+++ ch8/src/PassTwoDimensionalArray.java
@@ -1,9 +1,11 @@
+import java.util.Scanner;
+
 public class PassTwoDimensionalArray {
     public static void main(String[] args) {
 
     }
 
     public static int[][] getArray() {
-
+        Scanner input = new Scanner(System.in);
     }
 }
