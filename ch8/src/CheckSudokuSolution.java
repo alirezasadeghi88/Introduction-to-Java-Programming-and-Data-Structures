ee72e24d1a25e7b4d8526ed2084dcbed3643af17
alirezasadeghi88 <alirezasadeghi0558@gmail.com>
2026-09-05 06:57:38 +0330

    Add class CheckSudokuSolution

--- ch8/src/CheckSudokuSolution.java
+++ ch8/src/CheckSudokuSolution.java
@@ -14,5 +14,7 @@ public class CheckSudokuSolution {
         for (int i = 0; i < 9; i++)
             for (int j = 0; j < 9; j++)
                 grid[i][j] = input.nextInt();
+
+        return grid;
     }
 }
