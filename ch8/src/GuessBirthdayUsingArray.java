0893f4bf4b6efaeec93e0870f2c76ae9b4b57b9e
alirezasadeghi88 <alirezasadeghi0558@gmail.com>
2026-09-05 07:09:00 +0330

    Add class GuessBirthdayUsingArray

--- ch8/src/GuessBirthdayUsingArray.java
+++ ch8/src/GuessBirthdayUsingArray.java
@@ -28,5 +28,17 @@ public class GuessBirthdayUsingArray {
                         {28, 29, 30, 31}}};
 
     Scanner input = new Scanner(System.in);
+
+     for (int i = 0; i < 5; i++) {
+         System.out.println("Is your birthday in Set" + (i + 1) + "?");
+        for (int j = 0; j < 4; j++) {
+            for (int k = 0; k < 4; k++)
+                System.out.printf("%4d", dates[i][j][k]);
+            System.out.println();
+            System.out.print("\nEnter 0 for No and 1 for Yes: ");
+            answer = input.nextInt();
+            if (answer == 1)
+                day += dates[i][0][0];
+        }
     }
 }
