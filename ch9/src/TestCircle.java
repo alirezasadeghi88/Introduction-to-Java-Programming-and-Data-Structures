5c6eb760966156f93324bb1fd7b8f363c3681c78
alirezasadeghi88 <alirezasadeghi0558@gmail.com>
2026-09-06 06:43:39 +0330

    Add class Circle

--- ch9/src/TestCircle.java
+++ ch9/src/TestCircle.java
@@ -7,5 +7,7 @@ public class TestCircle {
 
 
         Circle circle2 = new Circle(25);
+        System.out.println("The area of the circle of radius "
+                + circle2.radius + "is" + circle2.getArea());
     }
 }
