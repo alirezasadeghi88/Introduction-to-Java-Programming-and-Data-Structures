659863b2beb21cfb5fbabb34fffb6cc64bb59724
alirezasadeghi88 <alirezasadeghi0558@gmail.com>
2026-09-06 07:01:42 +0330

    Add class TV

--- ch9/src/homework/TestTV.java
+++ ch9/src/homework/TestTV.java
@@ -3,5 +3,8 @@ package homework;
 public class TestTV {
     public static void main(String[] args) {
         TV tv1 = new TV();
+        tv1.turnOn();
+        tv1.setChannel(30);
+        tv1.setVolume(3);
     }
 }
