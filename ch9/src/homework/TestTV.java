205f171e4fcbe26782509c21acb26bbff972828d
alirezasadeghi88 <alirezasadeghi0558@gmail.com>
2026-09-06 07:02:56 +0330

    Add class Circle

--- ch9/src/homework/TestTV.java
+++ ch9/src/homework/TestTV.java
@@ -13,5 +13,8 @@ public class TestTV {
         tv2.channelUp();
         tv2.channelUp();
         tv2.volumeUp();
+
+        System.out.println("tv1's channel is " + tv1.channel
+                + " and volume level is " + tv1.volumeLevel);
     }
 }
