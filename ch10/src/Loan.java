72ca5165d6cf26c43125cc47af93355efdd7f154
alirezasadeghi88 <alirezasadeghi0558@gmail.com>
2026-09-07 12:17:45 +0330

    Add class Loan

--- ch10/src/Loan.java
+++ ch10/src/Loan.java
@@ -41,4 +41,9 @@ public class Loan {
         (1 / Math.pow(1 + monthlyInterestRate, numberOfYears * 12)));
         return monthlyPayment;
     }
+
+    public double getTotalPayment() {
+        double totalPayment = getMonthlyPayment() * numberOfYears * 12;
+        return totalPayment;
+    }
 }
