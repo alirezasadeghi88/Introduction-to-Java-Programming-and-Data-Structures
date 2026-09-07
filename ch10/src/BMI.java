324b0a8911111433883318bad41042b1fd32b8a9
alirezasadeghi88 <alirezasadeghi0558@gmail.com>
2026-09-07 12:20:30 +0330

    Add class BMI

--- ch10/src/BMI.java
+++ ch10/src/BMI.java
@@ -1,2 +1,8 @@
 public class BMI {
+    private String name;
+    private int age;
+    private double weight;
+    private double height;
+    public static final double KILOGRAMS_PER_POUND = 0.45359237;
+    public static final double METERS_PER_INCH = 0.0254;
 }
