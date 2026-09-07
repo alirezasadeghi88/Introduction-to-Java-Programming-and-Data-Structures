d016d654979704232396527649ce7740b422dd53
alirezasadeghi88 <alirezasadeghi0558@gmail.com>
2026-09-07 12:33:00 +0330

    Add class BMI

--- ch10/src/BMI.java
+++ ch10/src/BMI.java
@@ -5,4 +5,15 @@ public class BMI {
     private double height;
     public static final double KILOGRAMS_PER_POUND = 0.45359237;
     public static final double METERS_PER_INCH = 0.0254;
+
+    public BMI(String name, int age, double weight, double height) {
+        this.name = name;
+        this.age = age;
+        this.weight = weight;
+        this.height = height;
+    }
+
+    public BMI(String name, double weight, double height) {
+        this(name, 20, weight, height);
+    }
 }
