eabe6352f814f1746ba405d2e89d639c96aaa555
alirezasadeghi88 <alirezasadeghi0558@gmail.com>
2026-09-07 12:14:28 +0330

    Add class Loan

--- ch10/src/Loan.java
+++ ch10/src/Loan.java
@@ -3,4 +3,15 @@ public class Loan {
     private int numberOfYears;
     private double loanAmount;
     private java.util.Date loanDate;
+
+    public Loan() {
+        this(2.5, 1, 1000);
+    }
+
+    public Loan(double annualInterestRate, int numberOfYears, double loanAmount) {
+        this.annualInterestRate = annualInterestRate;
+        this.numberOfYears = numberOfYears;
+        this.loanAmount = loanAmount;
+        loanDate = new java.util.Date();
+    }
 }
