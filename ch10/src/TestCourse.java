57cb5b3205f8adb084dcf0f18a5f35acdd6bab58
alirezasadeghi88 <alirezasadeghi0558@gmail.com>
2026-09-08 06:24:47 +0330

    Add class Course

--- ch10/src/TestCourse.java
+++ ch10/src/TestCourse.java
@@ -2,5 +2,9 @@ public class TestCourse {
     public static void main(String[] args) {
         Course course1 = new Course("Data Structures");
         Course course2 = new Course("Database Systems");
+
+        course1.addStudent("Peter Jones");
+        course1.addStudent("Kim Smith");
+        course1.addStudent("Anne Kennedy");
     }
 }
