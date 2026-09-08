973a9027282e62d62430777c5ef893621f36b91b
alirezasadeghi88 <alirezasadeghi0558@gmail.com>
2026-09-08 06:51:03 +0330

    Add class StackOfIntegers

--- ch10/src/StackOfIntegers.java
+++ ch10/src/StackOfIntegers.java
@@ -2,4 +2,8 @@ public class StackOfIntegers {
     private int[] elements;
     private int size;
     public static final int DEFAULT_CAPACITY = 16;
+
+    public StackOfIntegers() {
+        this(DEFAULT_CAPACITY);
+    }
 }
