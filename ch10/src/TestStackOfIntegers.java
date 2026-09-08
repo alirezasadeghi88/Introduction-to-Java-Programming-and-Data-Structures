52b930746ff67d1c8bef8b36483ce03242db8a00
alirezasadeghi88 <alirezasadeghi0558@gmail.com>
2026-09-08 06:55:28 +0330

    Add class StackOfIntegers

--- ch10/src/TestStackOfIntegers.java
+++ ch10/src/TestStackOfIntegers.java
@@ -4,5 +4,7 @@ public class TestStackOfIntegers {
         for (int i = 0; i < 10; i++)
             stack.push(i);
 
+        while (!stack.empty())
+            System.out.print(stack.pop() + " ");
     }
 }
