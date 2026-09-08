f681c2c380efac3bd5d5ee696b923d41ae190912
alirezasadeghi88 <alirezasadeghi0558@gmail.com>
2026-09-08 06:52:07 +0330

    Add class StackOfIntegers

--- ch10/src/StackOfIntegers.java
+++ ch10/src/StackOfIntegers.java
@@ -10,4 +10,13 @@ public class StackOfIntegers {
     public StackOfIntegers(int capacity) {
         elements = new int[capacity];
     }
+
+    public void push(int value) {
+        if (size >= elements.length) {
+            int[] temp = new int[elements.length * 2];
+            System.arraycopy(elements, 0, temp, 0, elements.length);
+            elements = temp;
+        }
+        elements[size++] = value;
+    }
 }
