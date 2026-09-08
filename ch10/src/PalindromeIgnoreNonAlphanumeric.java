4414b9ad0c0dcb7ceeed9daca88e3593e35b643a
alirezasadeghi88 <alirezasadeghi0558@gmail.com>
2026-09-08 07:02:10 +0330

    Add class PalindromeIgnoreNonAlphanumeric

--- ch10/src/PalindromeIgnoreNonAlphanumeric.java
+++ ch10/src/PalindromeIgnoreNonAlphanumeric.java
@@ -1,5 +1,7 @@
+import java.util.Scanner;
+
 public class PalindromeIgnoreNonAlphanumeric {
     public static void main(String[] args) {
-
+        Scanner input = new Scanner(System.in);
     }
 }
