95bbfaecb5dca24022a31084b96fed0b4555c6c3
alirezasadeghi88 <alirezasadeghi0558@gmail.com>
2026-09-08 07:05:04 +0330

    Add class PalindromeIgnoreNonAlphanumeric

--- ch10/src/PalindromeIgnoreNonAlphanumeric.java
+++ ch10/src/PalindromeIgnoreNonAlphanumeric.java
@@ -14,6 +14,12 @@ public class PalindromeIgnoreNonAlphanumeric {
 
     public static String filter(String s) {
         StringBuilder stringBuilder = new StringBuilder();
+
+        for (int i = 0; i < s.length(); i++) {
+            if (Character.isLetterOrDigit(s.charAt(i))) {
+                stringBuilder.append(s.charAt(i));
+            }
+        }
     }
 
     public static String reverse(String s) {
