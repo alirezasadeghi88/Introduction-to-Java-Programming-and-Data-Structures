b1c76f21807690667d1bc1dae9729ab237dc098e
alirezasadeghi88 <alirezasadeghi0558@gmail.com>
2026-09-08 06:56:57 +0330

    Add class StackOfIntegers

--- ch10/src/LargeFactorial.java
+++ ch10/src/LargeFactorial.java
@@ -4,6 +4,7 @@ public class LargeFactorial {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
-
+        System.out.print("Enter an integer: ");
+            int n = input.nextInt();
     }
 }
