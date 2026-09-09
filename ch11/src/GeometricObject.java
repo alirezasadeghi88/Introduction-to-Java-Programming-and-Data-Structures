0cb27706c4ebf33a001ae58f15beaabe1c855487
alirezasadeghi88 <alirezasadeghi0558@gmail.com>
2026-09-09 06:34:49 +0330

    Add class GeometricObject

--- ch11/src/GeometricObject.java
+++ ch11/src/GeometricObject.java
@@ -2,4 +2,8 @@ public class GeometricObject {
     private String color = "white";
     private boolean filled;
     private java.util.Date dateCreated;
+
+    public GeometricObject() {
+        dateCreated = new java.util.Date();
+    }
 }
