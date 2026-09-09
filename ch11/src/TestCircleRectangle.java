f3cd4e5e2ce47e1c99ddf5f04126539db4c063f5
alirezasadeghi88 <alirezasadeghi0558@gmail.com>
2026-09-09 06:56:30 +0330

    Add class TestCircleRectangle

--- ch11/src/TestCircleRectangle.java
+++ ch11/src/TestCircleRectangle.java
@@ -2,5 +2,7 @@ public class TestCircleRectangle {
     public static void main(String[] args) {
         Circle circle = new Circle(1);
         System.out.println("A circle " + circle.toString());
+        System.out.println("The color is " + circle.getColor());
+        System.out.println("The radius is " + circle.getRadius());
     }
 }
