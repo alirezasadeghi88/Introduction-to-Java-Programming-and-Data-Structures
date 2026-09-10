1d8d74f9aa47e0135210cea520ddbb670fd82610
alirezasadeghi88 <alirezasadeghi0558@gmail.com>
2026-09-10 06:49:51 +0330

    Add class TestArrayList

--- ch11/src/TestArrayList.java
+++ ch11/src/TestArrayList.java
@@ -3,5 +3,7 @@ import java.util.ArrayList;
 public class TestArrayList {
     public static void main(String[] args) {
         ArrayList<String> cityList = new ArrayList<>();
+
+        cityList.add("London");
     }
 }
