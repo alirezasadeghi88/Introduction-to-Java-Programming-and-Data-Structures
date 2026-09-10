247841e83b0df85f10959efbae4811590d3b4ba0
alirezasadeghi88 <alirezasadeghi0558@gmail.com>
2026-09-10 06:54:20 +0330

    Add class TestArrayList

--- ch11/src/TestArrayList.java
+++ ch11/src/TestArrayList.java
@@ -15,6 +15,8 @@ public class TestArrayList {
         System.out.println("Is Miami in the list? " +
                 cityList.contains("Miami"));
         System.out.println("The location of Denver in the list? "
-                       + cityList.indexOf("Denver"));
+                + cityList.indexOf("Denver"));
+        System.out.println("Is the list empty? " +
+                cityList.isEmpty());
     }
 }
