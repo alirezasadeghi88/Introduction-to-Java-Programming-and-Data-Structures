494c05c5887e2c4843078a23a74ff9fa79d9c9ea
alirezasadeghi88 <alirezasadeghi0558@gmail.com>
2026-09-10 06:45:58 +0330

    Add class CastingDemo

--- ch11/src/CastingDemo.java
+++ ch11/src/CastingDemo.java
@@ -10,7 +10,8 @@ public class CastingDemo {
             System.out.println("The circle diameter is " +
                     ((Circle)object).getDiameter());
         }else if (object instanceof Rectangle) {
-
+            System.out.println("The rectangle area is " +
+                    ((Rectangle)object).getArea());
         }
     }
 }
