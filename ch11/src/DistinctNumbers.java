f7d9d97bfb5878a4ac2e9e0c0f12f6833ee76075
alirezasadeghi88 <alirezasadeghi0558@gmail.com>
2026-09-10 07:12:13 +0330

    Add class DistinctNumbers

--- ch11/src/DistinctNumbers.java
+++ ch11/src/DistinctNumbers.java
@@ -5,5 +5,7 @@ public class DistinctNumbers {
     public static void main(String[] args) {
         ArrayList<Integer> list = new ArrayList<>();
         Scanner input = new Scanner(System.in);
+        System.out.print("Enter integers (input ends with 0): ");
+        int value;
     }
 }
