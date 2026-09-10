27722b104d9ca9e7c476e4a7297a6a18a172382b
alirezasadeghi88 <alirezasadeghi0558@gmail.com>
2026-09-10 07:11:55 +0330

    Add class DistinctNumbers

--- ch11/src/DistinctNumbers.java
+++ ch11/src/DistinctNumbers.java
@@ -1,7 +1,9 @@
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class DistinctNumbers {
     public static void main(String[] args) {
         ArrayList<Integer> list = new ArrayList<>();
+        Scanner input = new Scanner(System.in);
     }
 }
