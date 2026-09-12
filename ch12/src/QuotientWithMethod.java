a7d150f6be59e7c21bd912e263b58c188371033a
alirezasadeghi88 <alirezasadeghi0558@gmail.com>
2026-09-12 06:41:57 +0330

    Add class QuotientWithMethod

--- ch12/src/QuotientWithMethod.java
+++ ch12/src/QuotientWithMethod.java
@@ -1,5 +1,8 @@
 public class QuotientWithMethod {
     public static int quotient(int number1, int number2) {
-
+        if (number2 == 0) {
+            System.out.println("Divisor cannot be zero");
+            System.exit(1);
+        }
     }
 }
