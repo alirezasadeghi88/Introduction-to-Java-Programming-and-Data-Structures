21e65d435a7c1a2b3315262e0e2aea45dc91ea0d
alirezasadeghi88 <alirezasadeghi0558@gmail.com>
2026-09-12 06:58:46 +0330

    Add class TestException

--- ch12/src/TestException.java
+++ ch12/src/TestException.java
@@ -3,6 +3,7 @@ public class TestException {
         try {
             System.out.println(sum(new int[] {1, 2, 3, 4, 5}));
         }catch (Exception ex) {
+            ex.printStackTrace();
 
         }
     }
