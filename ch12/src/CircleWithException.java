50091d9ecb82a840091463ea085db8771f82efc0
alirezasadeghi88 <alirezasadeghi0558@gmail.com>
2026-09-12 07:05:13 +0330

    Add class CircleWithException

--- ch12/src/CircleWithException.java
+++ ch12/src/CircleWithException.java
@@ -14,4 +14,12 @@ public class CircleWithException {
     public double getRadius() {
         return radius;
     }
+
+    public void setRadius(double newRadius)
+        throws IllegalArgumentException {
+        if (newRadius >= 0)
+            radius = newRadius;
+        else
+            throw new IllegalArgumentException("Radius cannot be negative");
+    }
 }
