eb410a41ea3043156ccb05aa224fabe81d719ed2
alirezasadeghi88 <alirezasadeghi0558@gmail.com>
2026-09-12 06:46:09 +0330

    Add class QuotientWithException

--- ch12/src/QuotientWithException.java
+++ ch12/src/QuotientWithException.java
@@ -1,5 +1,6 @@
 public class QuotientWithException {
     public static int quotient(int number1, int number2) {
-
+        if (number2 == 0)
+            throw new ArithmeticException("Divisor cannot be zero");
     }
 }
