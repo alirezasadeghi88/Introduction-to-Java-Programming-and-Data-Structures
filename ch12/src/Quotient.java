1feabef9fa0e3c5bcefa15e545cc826f58304dcf
alirezasadeghi88 <alirezasadeghi0558@gmail.com>
2026-09-12 06:37:53 +0330

    Add class Quotient

--- ch12/src/Quotient.java
+++ ch12/src/Quotient.java
@@ -7,5 +7,8 @@ public class Quotient {
         System.out.print("Enter two integers: ");
         int number1 = input.nextInt();
         int number2 = input.nextInt();
+        
+        System.out.println(number1 + " / " + number2 + " is " +
+                      (number1 / number2));
     }
 }
