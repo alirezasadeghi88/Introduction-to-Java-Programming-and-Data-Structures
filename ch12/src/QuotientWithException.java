17ca5fcc22fc627cf47e6f58914243362d5c5755
alirezasadeghi88 <alirezasadeghi0558@gmail.com>
2026-09-12 06:47:40 +0330

    Add class QuotientWithException

--- ch12/src/QuotientWithException.java
+++ ch12/src/QuotientWithException.java
@@ -10,5 +10,9 @@ public class QuotientWithException {
 
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter two integers: ");
+        int number1 = input.nextInt();
+        int number2 = input.nextInt();
     }
 }
