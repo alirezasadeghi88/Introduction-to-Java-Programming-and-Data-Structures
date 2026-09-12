1dc15b77e5ba69ae886ae9c2746be3528fbc0cfc
alirezasadeghi88 <alirezasadeghi0558@gmail.com>
2026-09-12 06:36:35 +0330

    Add class Quotient

--- ch12/src/Quotient.java
+++ ch12/src/Quotient.java
@@ -3,5 +3,9 @@ import java.util.Scanner;
 public class Quotient {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter two integers: ");
+        int number1 = input.nextInt();
+        int number2 = input.nextInt();
     }
 }
