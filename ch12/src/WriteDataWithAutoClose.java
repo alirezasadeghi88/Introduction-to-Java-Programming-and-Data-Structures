4e54a0a1be64eedf0d029f2287ccff715ffcda4e
alirezasadeghi88 <alirezasadeghi0558@gmail.com>
2026-09-13 07:27:00 +0330

    Add class WriteDataWithAutoClose

--- ch12/src/WriteDataWithAutoClose.java
+++ ch12/src/WriteDataWithAutoClose.java
@@ -11,5 +11,11 @@ public class WriteDataWithAutoClose {
         try (
         java.io.PrintWriter output = new java.io.PrintWriter(file);
             )
+        {
+            output.print("John T Smith ");
+            output.println(90);
+            output.print("Eric K Jones ");
+            output.println(85);
+        }
     }
 }
