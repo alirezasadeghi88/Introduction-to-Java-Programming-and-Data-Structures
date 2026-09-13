a3c0d2465eb85c83e3a500fe0455de2d150212c1
alirezasadeghi88 <alirezasadeghi0558@gmail.com>
2026-09-13 07:21:30 +0330

    Add class TestFileClass

--- ch12/src/TestFileClass.java
+++ ch12/src/TestFileClass.java
@@ -2,5 +2,6 @@ public class TestFileClass {
     public static void main(String[] args) {
         java.io.File file = new java.io.File("image/us.gif");
         System.out.println("Does it exist? " + file.exists());
+        System.out.println("The file has " + file.length() + " bytes");
     }
 }
