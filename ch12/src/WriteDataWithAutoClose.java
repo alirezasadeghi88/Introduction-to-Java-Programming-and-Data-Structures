bf81d36c142e49e480c29fdea80b53e94d43c03d
alirezasadeghi88 <alirezasadeghi0558@gmail.com>
2026-09-13 07:25:55 +0330

    Add class WriteDataWithAutoClose

--- ch12/src/WriteDataWithAutoClose.java
+++ ch12/src/WriteDataWithAutoClose.java
@@ -1,5 +1,10 @@
 public class WriteDataWithAutoClose {
     public static void main(String[] args) throws Exception {
         java.io.File file = new java.io.File("scores.txt");
+
+        if (file.exists()) {
+            System.out.println("File already exists");
+            System.exit(0);
+        }
     }
 }
