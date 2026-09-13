e5d8121c79a03d3c66d408449bd41c5d5619dfcd
alirezasadeghi88 <alirezasadeghi0558@gmail.com>
2026-09-13 07:22:04 +0330

    Add class TestFileClass

--- ch12/src/TestFileClass.java
+++ ch12/src/TestFileClass.java
@@ -4,5 +4,6 @@ public class TestFileClass {
         System.out.println("Does it exist? " + file.exists());
         System.out.println("The file has " + file.length() + " bytes");
         System.out.println("Can it be read? " + file.canRead());
+        System.out.println("Can it be written? " + file.canWrite());
     }
 }
