9db094b6a3e5611ee701b2960d51014e22829357
alirezasadeghi88 <alirezasadeghi0558@gmail.com>
2026-09-13 07:14:53 +0330

    Add class TestCircleWithCustomException

--- ch12/src/TestCircleWithCustomException.java
+++ ch12/src/TestCircleWithCustomException.java
@@ -1,7 +1,9 @@
 public class TestCircleWithCustomException {
     public static void main(String[] args) {
         try {
-
+            new CircleWithCustomException(5);
+            new CircleWithCustomException(-5);
+            new CircleWithCustomException(0);
         }
     }
 }
