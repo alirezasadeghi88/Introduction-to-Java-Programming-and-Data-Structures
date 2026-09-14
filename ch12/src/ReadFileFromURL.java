3db4b4b009ad76bc9de613a1e38b8b0d766d05e4
alirezasadeghi88 <alirezasadeghi0558@gmail.com>
2026-09-14 11:53:53 +0330

    Add class ReadFileFromURL

--- ch12/src/ReadFileFromURL.java
+++ ch12/src/ReadFileFromURL.java
@@ -14,6 +14,8 @@ public class ReadFileFromURL {
                 String line = input.nextLine();
                 count += line.length();
             }
+
+            System.out.println("The file size is " + count + " characters");
         }
     }
 }
