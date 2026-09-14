8ad82c1b19f9c3e82588fb16c077ddac236397f7
alirezasadeghi88 <alirezasadeghi0558@gmail.com>
2026-09-14 11:52:14 +0330

    Add class ReadFileFromURL

--- ch12/src/ReadFileFromURL.java
+++ ch12/src/ReadFileFromURL.java
@@ -1,5 +1,8 @@
+import java.util.Scanner;
+
 public class ReadFileFromURL {
     public static void main(String[] args) {
         System.out.print("Enter a URL: ");
+        String URLString = new Scanner(System.in).next();
     }
 }
