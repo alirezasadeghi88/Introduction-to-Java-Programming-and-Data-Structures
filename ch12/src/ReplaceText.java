02f4e2f02c8dbd97a3589506004c07dfd9c5b19c
alirezasadeghi88 <alirezasadeghi0558@gmail.com>
2026-09-14 11:43:50 +0330

    Add class ReplaceText

--- ch12/src/ReplaceText.java
+++ ch12/src/ReplaceText.java
@@ -2,6 +2,7 @@ public class ReplaceText {
     public static void main(String[] args) throws Exception {
         if (args.length != 4) {
             System.out.println("Usage: java ReplaceText sourceFile targetFile oldStr newStr");
+            System.exit(1);
         }
     }
 }
