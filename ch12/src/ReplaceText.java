89197b11f498f9f4456965b1674c97bbf44d2a3e
alirezasadeghi88 <alirezasadeghi0558@gmail.com>
2026-09-14 11:48:04 +0330

    Add class ReplaceText

--- ch12/src/ReplaceText.java
+++ ch12/src/ReplaceText.java
@@ -27,6 +27,7 @@ public class ReplaceText {
         ){
             while (input.hasNext()) {
                 String s1 = input.nextLine();
+                String s2 = s1.replaceAll(args[2], args[3]);
             }
     }
 }
