60fa09f03ed362995509351363476567e251faa0
alirezasadeghi88 <alirezasadeghi0558@gmail.com>
2026-09-14 11:44:16 +0330

    Add class ReplaceText

--- ch12/src/ReplaceText.java
+++ ch12/src/ReplaceText.java
@@ -1,8 +1,12 @@
+import java.io.File;
+
 public class ReplaceText {
     public static void main(String[] args) throws Exception {
         if (args.length != 4) {
             System.out.println("Usage: java ReplaceText sourceFile targetFile oldStr newStr");
             System.exit(1);
         }
+
+        File sourceFile = new File(args[0]);
     }
 }
