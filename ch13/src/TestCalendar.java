584a843857c1590d3dbf51d4105383468d90a85d
alirezasadeghi88 <alirezasadeghi0558@gmail.com>
2026-09-15 07:22:45 +0330

    Add class TestCalendar

--- ch13/src/TestCalendar.java
+++ ch13/src/TestCalendar.java
@@ -13,5 +13,20 @@ public class TestCalendar {
         System.out.println("HOUR: " + calendar.get(Calendar.HOUR));
         System.out.println("HOUR_OF_DAY: " +
                 calendar.get(Calendar.HOUR_OF_DAY));
+        System.out.println("MINUTE: " +
+                calendar.get(Calendar.MINUTE));
+        System.out.println("SECOND: " +
+                calendar.get(Calendar.SECOND));
+        System.out.println("DAY_OF_WEEK: " +
+                calendar.get(Calendar.DAY_OF_WEEK));
+        System.out.println("DAY_OF_MONTH: " +
+                calendar.get(Calendar.DAY_OF_MONTH));
+        System.out.println("DAY_OF_YEAR: " +
+                calendar.get(Calendar.DAY_OF_YEAR));
+        System.out.println("WEEK_OF_MONTH: " +
+                calendar.get(Calendar.WEEK_OF_MONTH));
+        System.out.println("WEEK_OF_YEAR: " +
+                calendar.get(Calendar.WEEK_OF_YEAR));
+        System.out.println("AM_PM: " + calendar.get(Calendar.AM_PM));
     }
 }
