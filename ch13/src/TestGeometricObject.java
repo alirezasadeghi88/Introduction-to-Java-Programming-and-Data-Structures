4681460b817f3cd074a27ad2eb26e1f98603d5e8
alirezasadeghi88 <alirezasadeghi0558@gmail.com>
2026-09-15 07:14:19 +0330

    Add class TestGeometricObject

--- ch13/src/TestGeometricObject.java
+++ ch13/src/TestGeometricObject.java
@@ -11,6 +11,8 @@ public class TestGeometricObject {
     }
 
     public static void displayGeometricObject(GeometricObject object) {
-
+        System.out.println();
+        System.out.println("The area is " + object.getArea());
+        System.out.println("The perimeter is " + object.getPerimeter());
     }
 }
