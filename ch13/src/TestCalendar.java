b60cdf951a84122ff9e30fa16e83c28e736feff6
alirezasadeghi88 <alirezasadeghi0558@gmail.com>
2026-09-15 07:21:37 +0330

    Add class TestCalendar

--- ch13/src/TestCalendar.java
+++ ch13/src/TestCalendar.java
@@ -8,6 +8,10 @@ public class TestCalendar {
 
         System.out.println("Current time is " + new Date());
         System.out.println("YEAR: " + calendar.get(Calendar.YEAR));
-
+        System.out.println("MONTH: " + calendar.get(Calendar.MONTH));
+        System.out.println("DATE: " + calendar.get(Calendar.DATE));
+        System.out.println("HOUR: " + calendar.get(Calendar.HOUR));
+        System.out.println("HOUR_OF_DAY: " +
+                calendar.get(Calendar.HOUR_OF_DAY));
     }
 }
