11e9c0d3f2f9d6dbe322f0b584eb280f8a22dc8e
alirezasadeghi88 <alirezasadeghi0558@gmail.com>
2026-09-15 07:16:49 +0330

    Add class LargestNumber

--- ch13/src/LargestNumber.java
+++ ch13/src/LargestNumber.java
@@ -3,5 +3,7 @@ import java.util.ArrayList;
 public class LargestNumber {
     public static void main(String[] args) {
         ArrayList<Number> list = new ArrayList<>();
+        list.add(45);
+        list.add(3445.53);
     }
 }
