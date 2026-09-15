da4ff02409bfbc91e6c6311b22a462656221305d
alirezasadeghi88 <alirezasadeghi0558@gmail.com>
2026-09-15 07:13:18 +0330

    Add class TestGeometricObject

--- ch13/src/TestGeometricObject.java
+++ ch13/src/TestGeometricObject.java
@@ -1,5 +1,12 @@
+import java.awt.*;
+
 public class TestGeometricObject {
     public static void main(String[] args) {
+        GeometricObject geoObject1 = new Circle(5);
+        GeometricObject geoObject2 = new Rectangle(5, 3);
+    }
+
+    public static boolean equalArea(GeometricObject object1, GeometricObject object2) {
 
     }
 }
