c8cee2f233ee6f13f8c167ac96758a310f2fbc19
alirezasadeghi88 <alirezasadeghi0558@gmail.com>
2026-09-16 08:55:19 +0330

    Add class Rational

--- ch13/src/TestRationalClass.java
+++ ch13/src/TestRationalClass.java
@@ -2,5 +2,7 @@ public class TestRationalClass {
     public static void main(String[] args) {
         Rational r1 = new Rational(4, 2);
         Rational r2 = new Rational(2, 3);
+
+        System.out.println(r1 + " + " + r2 + " = " + r1.add(r2));
     }
 }
