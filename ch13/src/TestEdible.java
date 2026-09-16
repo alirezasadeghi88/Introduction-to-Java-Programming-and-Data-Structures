476b77f49c3afa5d84ab383dc45b88f65d1f4e01
alirezasadeghi88 <alirezasadeghi0558@gmail.com>
2026-09-16 08:23:00 +0330

    Add class TestEdible

--- ch13/src/TestEdible.java
+++ ch13/src/TestEdible.java
@@ -6,6 +6,11 @@ public class TestEdible {
         for (int i = 0; i < objects.length; i++) {
             if (objects[i] instanceof Edible)
                 System.out.println(((Edible)objects[i]).howToEat());
+
+
+            if (objects[i] instanceof Animal) {
+
+            }
         }
     }
 }
