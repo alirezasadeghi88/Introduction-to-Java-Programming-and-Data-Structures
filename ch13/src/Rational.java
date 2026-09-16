fe98b56910bb710014270385b2368f2bfc39dbce
alirezasadeghi88 <alirezasadeghi0558@gmail.com>
2026-09-16 08:48:50 +0330

    Add class Rational

--- ch13/src/Rational.java
+++ ch13/src/Rational.java
@@ -61,6 +61,7 @@ public class Rational extends Number implements Comparable<Rational> {
 
     @Override
     public String toString() {
-
+        if (denominator == 1)
+            return numerator + "";
     }
 }
