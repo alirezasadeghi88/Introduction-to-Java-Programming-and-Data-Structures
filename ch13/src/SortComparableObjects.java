cea58f0a2a1561dda28e8a476d3e625c1bb3fe80
alirezasadeghi88 <alirezasadeghi0558@gmail.com>
2026-09-16 08:30:50 +0330

    Add class SortComparableObjects

--- ch13/src/SortComparableObjects.java
+++ ch13/src/SortComparableObjects.java
@@ -2,5 +2,6 @@ public class SortComparableObjects {
     public static void main(String[] args) {
         String[] cities = {"Savannah", "Boston", "Atlanta", "Tampa"};
         java.util.Arrays.sort(cities);
+        for (String city: cities)
     }
 }
