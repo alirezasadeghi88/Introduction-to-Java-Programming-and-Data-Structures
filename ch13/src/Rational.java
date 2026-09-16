44c9450c9f481f825887f818e4bb8e7b635cc62b
alirezasadeghi88 <alirezasadeghi0558@gmail.com>
2026-09-16 08:46:39 +0330

    Add class Rational

--- ch13/src/Rational.java
+++ ch13/src/Rational.java
@@ -46,4 +46,8 @@ public class Rational extends Number implements Comparable<Rational> {
         long d = denominator * secondRational.getDenominator();
         return new Rational(n, d);
     }
+
+    public Rational multiply(Rational secondRational) {
+
+    }
 }
