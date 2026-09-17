926f817699d15ab98ae5f20b01748ea40ed564c4
alirezasadeghi88 <alirezasadeghi0558@gmail.com>
2026-09-17 09:04:14 +0330

    Add class TestObjectInputStream

--- ch17/src/TestObjectInputStream.java
+++ ch17/src/TestObjectInputStream.java
@@ -3,6 +3,8 @@ import java.io.IOException;
 public class TestObjectInputStream {
     public static void main(String[] args)
             throws ClassNotFoundException, IOException {
+        try (
 
+                )
     }
 }
