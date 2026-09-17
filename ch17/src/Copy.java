32404c9c99361e5fb25ba5b89973dcfc29a62eb7
alirezasadeghi88 <alirezasadeghi0558@gmail.com>
2026-09-17 08:57:12 +0330

    Add class Copy

--- ch17/src/Copy.java
+++ ch17/src/Copy.java
@@ -16,8 +16,10 @@ public class Copy {
         }
 
         File targetFile = new File(args[1]);
-        System.out.println("Target file " + args[1]
-                + " already exists");
-        System.exit(3);
+        if (targetFile.exists()) {
+            System.out.println("Target file " + args[1]
+                    + " already exists");
+            System.exit(3);
+        }
     }
 }
