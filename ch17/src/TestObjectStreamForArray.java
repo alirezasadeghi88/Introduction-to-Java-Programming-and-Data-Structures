248b11b473cfd42eea39a2a34a607818fc8a6cec
alirezasadeghi88 <alirezasadeghi0558@gmail.com>
2026-09-17 09:06:58 +0330

    Add class TestObjectStreamForArray

--- ch17/src/TestObjectStreamForArray.java
+++ ch17/src/TestObjectStreamForArray.java
@@ -1,6 +1,8 @@
+import java.io.IOException;
+
 public class TestObjectStreamForArray {
     public static void main(String[] args)
-            throws ClassNotFoundException, IOException  {
-
+            throws ClassNotFoundException, IOException {
+        int[] numbers = {1, 2, 3, 4, 5};
     }
 }
