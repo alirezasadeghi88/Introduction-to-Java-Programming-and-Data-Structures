6519a3dd2cd56f216ab307f53c0e32e65e68a0be
alirezasadeghi88 <alirezasadeghi0558@gmail.com>
2026-09-17 08:54:05 +0330

    Add class Copy

--- ch17/src/Copy.java
+++ ch17/src/Copy.java
@@ -3,7 +3,7 @@ import java.io.IOException;
 public class Copy {
     public static void main(String[] args) throws IOException {
         if (args.length != 2) {
-
+            System.out.println("Usage: java Copy sourceFile targetfile");
         }
     }
 }
