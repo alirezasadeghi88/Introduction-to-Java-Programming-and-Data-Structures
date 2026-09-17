29dda985aefd41b37e3ab6359ae9ae9aef33b107
alirezasadeghi88 <alirezasadeghi0558@gmail.com>
2026-09-17 09:08:29 +0330

    Add class TestObjectStreamForArray

--- ch17/src/TestObjectStreamForArray.java
+++ ch17/src/TestObjectStreamForArray.java
@@ -15,5 +15,9 @@ public class TestObjectStreamForArray {
             output.writeObject(numbers);
             output.writeObject(strings);
         }
+
+        try (
+
+                )
     }
 }
