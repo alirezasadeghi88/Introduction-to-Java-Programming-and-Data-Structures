60575702c321f19d74bcf35ab51ff694ac697dc8
alirezasadeghi88 <alirezasadeghi0558@gmail.com>
2026-09-17 08:41:42 +0330

    Add class TestFileStream

--- ch17/src/TestFileStream.java
+++ ch17/src/TestFileStream.java
@@ -5,6 +5,8 @@ public class TestFileStream {
     public static void main(String[] args)throws IOException {
         try (
                 FileOutputStream output = new FileOutputStream("temp.dat");
-                )
+                ) {
+
+        }
     }
 }
