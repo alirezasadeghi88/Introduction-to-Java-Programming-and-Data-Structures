a331c8ce307f523a28078b57a7b529a0c696c208
alirezasadeghi88 <alirezasadeghi0558@gmail.com>
2026-09-17 09:04:30 +0330

    Add class TestObjectInputStream

--- ch17/src/TestObjectInputStream.java
+++ ch17/src/TestObjectInputStream.java
@@ -1,10 +1,13 @@
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.ObjectInputStream;
 
 public class TestObjectInputStream {
     public static void main(String[] args)
             throws ClassNotFoundException, IOException {
         try (
-
+                ObjectInputStream input =
+                        new ObjectInputStream(new FileInputStream("object.dat"));
                 )
     }
 }
