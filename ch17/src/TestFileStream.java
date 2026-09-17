3b5525dd7011040c3a75ad1791b59e5f2815fe68
alirezasadeghi88 <alirezasadeghi0558@gmail.com>
2026-09-17 08:41:21 +0330

    Add class TestFileStream

--- ch17/src/TestFileStream.java
+++ ch17/src/TestFileStream.java
@@ -1,9 +1,10 @@
+import java.io.FileOutputStream;
 import java.io.IOException;
 
 public class TestFileStream {
     public static void main(String[] args)throws IOException {
         try (
-
+                FileOutputStream output = new FileOutputStream("temp.dat");
                 )
     }
 }
