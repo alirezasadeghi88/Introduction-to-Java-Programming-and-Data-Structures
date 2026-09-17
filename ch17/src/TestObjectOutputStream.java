014756fd92daaa0a5d692d39dcbf1cff04f0d532
alirezasadeghi88 <alirezasadeghi0558@gmail.com>
2026-09-17 09:02:07 +0330

    Add class TestObjectOutputStream

--- ch17/src/TestObjectOutputStream.java
+++ ch17/src/TestObjectOutputStream.java
@@ -1,9 +1,12 @@
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.ObjectOutputStream;
 
 public class TestObjectOutputStream {
     public static void main(String[] args) throws IOException {
         try (
-
+                ObjectOutputStream output =
+                        new ObjectOutputStream(new FileOutputStream("object.dat"));
                 )
     }
 }
