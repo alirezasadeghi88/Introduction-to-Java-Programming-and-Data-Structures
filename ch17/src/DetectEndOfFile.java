72db529e238acc3f48c51dbd84d71068ad97ac45
alirezasadeghi88 <alirezasadeghi0558@gmail.com>
2026-09-17 08:50:51 +0330

    Add class DetectEndOfFile

--- ch17/src/DetectEndOfFile.java
+++ ch17/src/DetectEndOfFile.java
@@ -1,4 +1,6 @@
+import java.io.DataInputStream;
 import java.io.DataOutputStream;
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
 
 public class DetectEndOfFile {
@@ -10,6 +12,9 @@ public class DetectEndOfFile {
                 output.writeDouble(43.25);
                 output.writeDouble(3.2);
             }
+
+            try (DataInputStream input =
+                         new DataInputStream(new FileInputStream("test.dat"))) {
         }
     }
 }
