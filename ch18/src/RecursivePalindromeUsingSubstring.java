0c57a8cd746335e36430160de51532476b42b5e4
alirezasadeghi88 <alirezasadeghi0558@gmail.com>
2026-09-19 08:50:00 +0330

    Add class RecursivePalindromeUsingSubstring

--- ch18/src/RecursivePalindromeUsingSubstring.java
+++ ch18/src/RecursivePalindromeUsingSubstring.java
@@ -5,10 +5,11 @@ public class RecursivePalindromeUsingSubstring {
         else if (s.charAt(0) != s.charAt(s.length() -  1))
             return false;
         else
-            return isPalindrome(s.substring(1, s.length() − 1));
+            return isPalindrome(s.substring(1, s.length() - 1));
     }
 
     public static void main(String[] args) {
-
+        System.out.println("Is moon a palindrome? "
+                + isPalindrome("moon"));
     }
 }
