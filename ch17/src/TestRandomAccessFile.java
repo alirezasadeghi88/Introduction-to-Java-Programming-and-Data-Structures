25cf3751afb36c8459ae019242a59091eae29057
alirezasadeghi88 <alirezasadeghi0558@gmail.com>
2026-09-19 08:31:43 +0330

    Add class TestRandomAccessFile

--- ch17/src/TestRandomAccessFile.java
+++ ch17/src/TestRandomAccessFile.java
@@ -1,9 +1,10 @@
 import java.io.IOException;
+import java.io.RandomAccessFile;
 
 public class TestRandomAccessFile {
     public static void main(String[] args) throws IOException {
         try (
-
+                RandomAccessFile inout = new RandomAccessFile("inout.dat", "rw");
                 )
     }
 }
