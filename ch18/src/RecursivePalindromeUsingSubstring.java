8f64e84c04275b311f6928ca727c12dc8daea48a
alirezasadeghi88 <alirezasadeghi0558@gmail.com>
2026-09-19 08:48:45 +0330

    Add class RecursivePalindromeUsingSubstring

--- ch18/src/RecursivePalindromeUsingSubstring.java
+++ ch18/src/RecursivePalindromeUsingSubstring.java
@@ -1,5 +1,7 @@
 public class RecursivePalindromeUsingSubstring {
     public static boolean isPalindrome(String s) {
         if (s.length() <= 1)
+            return true;
+        else if (s.charAt(0) != s.charAt(s.length() −  1))
     }
 }
