019bfdcab1298e3a79d71df2360cf31b6c3c57bd
alirezasadeghi88 <alirezasadeghi0558@gmail.com>
2026-09-19 08:43:40 +0330

    Add class ComputeFibonacci

--- ch18/src/ComputeFibonacci.java
+++ ch18/src/ComputeFibonacci.java
@@ -3,5 +3,7 @@ import java.util.Scanner;
 public class ComputeFibonacci {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter an index for a Fibonacci number: ");
     }
 }
