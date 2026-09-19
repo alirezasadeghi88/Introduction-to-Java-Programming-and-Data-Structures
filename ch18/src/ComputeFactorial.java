0ca65dd1a784421ae4f9efbf626f0a9648761692
alirezasadeghi88 <alirezasadeghi0558@gmail.com>
2026-09-19 08:40:49 +0330

    Add class ComputeFactorial

--- ch18/src/ComputeFactorial.java
+++ ch18/src/ComputeFactorial.java
@@ -3,5 +3,7 @@ import java.util.Scanner;
 public class ComputeFactorial {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter a nonnegative integer: ");
     }
 }
