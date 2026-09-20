44dbd8376d25967f5efab79128d9daad4c85e4c6
alirezasadeghi88 <alirezasadeghi0558@gmail.com>
2026-09-20 09:18:37 +0330

    Add class ComputeFactorialTailRecursion

--- ch18/src/ComputeFactorialTailRecursion.java
+++ ch18/src/ComputeFactorialTailRecursion.java
@@ -2,4 +2,8 @@ public class ComputeFactorialTailRecursion {
     public static long factorial(int n) {
         return factorial(n, 1);
     }
+
+    private static long factorial(int n, int result) {
+
+    }
 }
