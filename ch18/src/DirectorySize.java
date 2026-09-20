162c200157bc48ac4c9237e9fe605b6b3b7d30a5
alirezasadeghi88 <alirezasadeghi0558@gmail.com>
2026-09-20 09:05:28 +0330

    Add class DirectorySize

--- ch18/src/DirectorySize.java
+++ ch18/src/DirectorySize.java
@@ -4,5 +4,6 @@ public class DirectorySize {
     public static void main(String[] args) {
         System.out.print("Enter a directory or a file: ");
         Scanner input = new Scanner(System.in);
+        String directory = input.nextLine();
     }
 }
