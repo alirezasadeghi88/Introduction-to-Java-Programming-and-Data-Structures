4e032520a9dc29348820a6000ec1d8e5242166da
alirezasadeghi88 <alirezasadeghi0558@gmail.com>
2026-09-20 09:14:22 +0330

    Add class TowerOfHanoi

--- ch18/src/TowerOfHanoi.java
+++ ch18/src/TowerOfHanoi.java
@@ -3,5 +3,6 @@ import java.util.Scanner;
 public class TowerOfHanoi {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+        System.out.print("Enter number of disks: ");
     }
 }
