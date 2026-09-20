6a5b95004b170a3bac8eab9e54da07ec96fb388a
alirezasadeghi88 <alirezasadeghi0558@gmail.com>
2026-09-20 09:01:26 +0330

    Add class RecursiveBinarySearch

--- ch18/src/RecursiveBinarySearch.java
+++ ch18/src/RecursiveBinarySearch.java
@@ -2,5 +2,6 @@ public class RecursiveBinarySearch {
     public static int binarySearch(int[] list, int key) {
         int low = 0;
         int high = list.length - 1;
+        return binarySearch(list, key, low, high);
     }
 }
