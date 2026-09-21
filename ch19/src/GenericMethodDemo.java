5320a4e30a6d41b54854042b866770755f9b5583
alirezasadeghi88 <alirezasadeghi0558@gmail.com>
2026-09-21 13:09:09 +0330

    Add class GenericMethodDemo

--- ch19/src/GenericMethodDemo.java
+++ ch19/src/GenericMethodDemo.java
@@ -1,5 +1,6 @@
 public class GenericMethodDemo {
     public static void main(String[] args) {
         Integer[] integers = {1, 2, 3, 4, 5};
+        String[] strings = {"London", "Paris", "New York", "Austin"};
     }
 }
