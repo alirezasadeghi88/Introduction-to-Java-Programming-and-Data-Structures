33f1c485745e2ab3f2b3a02e32f09e5ffaebea96
alirezasadeghi88 <alirezasadeghi0558@gmail.com>
2026-09-21 13:12:07 +0330

    Add class GenericSort

--- ch19/src/GenericSort.java
+++ ch19/src/GenericSort.java
@@ -1,5 +1,6 @@
 public class GenericSort {
     public static void main(String[] args) {
-
+        Integer[] intArray = {Integer.valueOf(2), Integer.valueOf(4),
+                Integer.valueOf(3)};
     }
 }
