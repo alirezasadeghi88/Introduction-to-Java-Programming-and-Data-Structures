3ec3f376a064e33d975c4d9c63455c6578d3c507
alirezasadeghi88 <alirezasadeghi0558@gmail.com>
2026-09-21 13:17:25 +0330

    Add class BoundedTypeDemo

--- ch19/src/GenericSort.java
+++ ch19/src/GenericSort.java
@@ -37,6 +37,7 @@ public class GenericSort {
             for (int j = i + 1; j < list.length; j++) {
                 if (currentMin.compareTo(list[j]) > 0) {
                     currentMin = list[j];
+                    currentMinIndex = j;
                 }
             }
         }
