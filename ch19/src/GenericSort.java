9b441bcef251383b234b3e69d0d2154d4bb59182
alirezasadeghi88 <alirezasadeghi0558@gmail.com>
2026-09-21 13:18:27 +0330

    Add class BoundedTypeDemo

--- ch19/src/GenericSort.java
+++ ch19/src/GenericSort.java
@@ -50,5 +50,6 @@ public class GenericSort {
 
     public static void printList(Object[] list) {
         for (int i = 0; i < list.length; i++)
+            System.out.print(list[i] + " ");
     }
 }
