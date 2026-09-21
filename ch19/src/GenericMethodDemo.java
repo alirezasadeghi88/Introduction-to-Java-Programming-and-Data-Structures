c9763174b3101d0ac55519612c2e2281634b48e6
alirezasadeghi88 <alirezasadeghi0558@gmail.com>
2026-09-21 13:10:13 +0330

    Add class GenericMethodDemo

--- ch19/src/GenericMethodDemo.java
+++ ch19/src/GenericMethodDemo.java
@@ -11,5 +11,6 @@ public class GenericMethodDemo {
     public static <E> void print(E[] list) {
 
         for (int i = 0; i < list.length; i++)
+            System.out.print(list[i] + " ");
     }
 }
