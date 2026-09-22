7be035b43bcf8cf97dc5a595f4f39f9aea554b34
alirezasadeghi88 <alirezasadeghi0558@gmail.com>
2026-09-22 09:19:28 +0330

    Add class GenericMatrix

--- ch19/src/GenericMatrix.java
+++ ch19/src/GenericMatrix.java
@@ -1,4 +1,5 @@
 public abstract class GenericMatrix<E extends Number> {
     protected abstract E add(E o1, E o2);
     protected abstract E multiply(E o1, E o2);
+    protected abstract E zero();
 }
