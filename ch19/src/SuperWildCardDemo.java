bb232e4869681a048541c5ab54d40c337ceb636b
alirezasadeghi88 <alirezasadeghi0558@gmail.com>
2026-09-22 09:11:50 +0330

    Add class SuperWildCardDemo

--- ch19/src/SuperWildCardDemo.java
+++ ch19/src/SuperWildCardDemo.java
@@ -1,5 +1,6 @@
 public class SuperWildCardDemo {
     public static void main(String[] args) {
-
+        GenericStack<String> stack1 = new GenericStack<>();
+        GenericStack<Object> stack2 = new GenericStack<>();
     }
 }
