1b42912c161aa62134cd98b0cbfd7f1018721d59
alirezasadeghi88 <alirezasadeghi0558@gmail.com>
2026-09-22 09:35:44 +0330

    Add class TestIntegerMatrix

--- ch19/src/TestIntegerMatrix.java
+++ ch19/src/TestIntegerMatrix.java
@@ -1,5 +1,6 @@
 public class TestIntegerMatrix {
     public static void main(String[] args) {
         Integer[][] m1 = new Integer[][]{{1, 2, 3}, {4, 5, 6}, {1, 1, 1}};
+        Integer[][] m2 = new Integer[][]{{1, 1, 1}, {2, 2, 2}, {0, 0, 0}};
     }
 }
