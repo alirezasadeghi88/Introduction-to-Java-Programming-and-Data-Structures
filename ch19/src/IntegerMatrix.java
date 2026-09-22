dca9de340f6fb0f9a0dac4b73a14cac1125296d2
alirezasadeghi88 <alirezasadeghi0558@gmail.com>
2026-09-22 09:29:39 +0330

    Add class IntegerMatrix

--- ch19/src/IntegerMatrix.java
+++ ch19/src/IntegerMatrix.java
@@ -2,7 +2,7 @@
 public class IntegerMatrix extends GenericMatrix<Integer> {
     @Override
     protected Integer add(Integer o1, Integer o2) {
-        return 0;
+        return o1 + o2;
     }
 
 }
