3097299f12099b05a371242cdb3ebf0c0c6aea3a
alirezasadeghi88 <alirezasadeghi0558@gmail.com>
2026-09-22 09:23:47 +0330

    Add class GenericMatrix

--- ch19/src/GenericMatrix.java
+++ ch19/src/GenericMatrix.java
@@ -31,7 +31,9 @@ public abstract class GenericMatrix<E extends Number> {
                 (E[][])new Number[matrix1.length][matrix2[0].length];
 
         for (int i = 0; i < result.length; i++) {
+            for (int j = 0; j < result[0].length; j++) {
 
+            }
         }
     }
 }
