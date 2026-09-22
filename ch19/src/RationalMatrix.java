cc44e0420eee7b949cc6a721bee8cdf6a6473737
alirezasadeghi88 <alirezasadeghi0558@gmail.com>
2026-09-22 09:32:29 +0330

    Add class RationalMatrix

--- ch19/src/RationalMatrix.java
+++ ch19/src/RationalMatrix.java
@@ -9,4 +9,9 @@ public class RationalMatrix extends GenericMatrix<Rational> {
     protected Rational multiply(Rational o1, Rational o2) {
         return r1.multiply(r2);
     }
+
+    @Override
+    protected Rational zero() {
+        return null;
+    }
 }
