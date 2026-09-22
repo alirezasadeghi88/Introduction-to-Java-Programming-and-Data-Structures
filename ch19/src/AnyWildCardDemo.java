6b38af762bdb181218becf3a5d25efcbdd05f9d3
alirezasadeghi88 <alirezasadeghi0558@gmail.com>
2026-09-22 09:08:59 +0330

    Add class AnyWildCardDemo

--- ch19/src/AnyWildCardDemo.java
+++ ch19/src/AnyWildCardDemo.java
@@ -2,5 +2,6 @@ public class AnyWildCardDemo {
     public static void main(String[] args) {
         GenericStack<Integer> intStack = new GenericStack<>();
         intStack.push(1);
+        intStack.push(2);
     }
 }
