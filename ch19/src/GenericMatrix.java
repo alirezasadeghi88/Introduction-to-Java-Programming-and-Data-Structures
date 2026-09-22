b58167635e792f6afc93a95f23fea14e4e0188e0
alirezasadeghi88 <alirezasadeghi0558@gmail.com>
2026-09-22 09:22:35 +0330

    Add class GenericMatrix

--- ch19/src/GenericMatrix.java
+++ ch19/src/GenericMatrix.java
@@ -22,6 +22,8 @@ public abstract class GenericMatrix<E extends Number> {
     }
 
     public E[][] multiplyMatrix(E[][] matrix1, E[][] matrix2) {
+        if (matrix1[0].length != matrix2.length) {
 
+        }
     }
 }
