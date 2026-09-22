62b19ef418b81e14f2cc93da817a34d8b208c455
alirezasadeghi88 <alirezasadeghi0558@gmail.com>
2026-09-22 09:20:30 +0330

    Add class GenericMatrix

--- ch19/src/GenericMatrix.java
+++ ch19/src/GenericMatrix.java
@@ -6,7 +6,8 @@ public abstract class GenericMatrix<E extends Number> {
     public E[][] addMatrix(E[][] matrix1, E[][] matrix2) {
         if ((matrix1.length != matrix2.length) ||
                 (matrix1[0].length != matrix2[0].length)) {
-
+            throw new RuntimeException(
+                    "The matrices do not have the same size");
         }
     }
 }
