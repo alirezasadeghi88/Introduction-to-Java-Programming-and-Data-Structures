8e0e8c9b28b2adc2963151cc9b6d99f4838cf935
alirezasadeghi88 <alirezasadeghi0558@gmail.com>
2026-09-22 09:26:01 +0330

    Add class GenericMatrix

--- ch19/src/GenericMatrix.java
+++ ch19/src/GenericMatrix.java
@@ -51,6 +51,7 @@ public abstract class GenericMatrix<E extends Number> {
                 System.out.print(" " + m1[i][j]);
 
             if (i == m1.length / 2)
+                System.out.print("  " + op + "  ");
         }
     }
 }
