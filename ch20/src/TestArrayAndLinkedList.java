6e6ef0571f5695764037a40b7f94aedf924acd38
alirezasadeghi88 <alirezasadeghi0558@gmail.com>
2026-09-23 13:11:06 +0330

    Add class TestArrayAndLinkedList

--- ch20/src/TestArrayAndLinkedList.java
+++ ch20/src/TestArrayAndLinkedList.java
@@ -1,5 +1,8 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class TestArrayAndLinkedList {
     public static void main(String[] args) {
-
+        List<Integer> arrayList = new ArrayList<>();
     }
 }
