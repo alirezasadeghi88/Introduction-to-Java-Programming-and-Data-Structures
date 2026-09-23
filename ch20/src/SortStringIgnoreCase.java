3760c69cc586128151cbb647024fffd509f52fd5
alirezasadeghi88 <alirezasadeghi0558@gmail.com>
2026-09-23 13:20:39 +0330

    Add class SortStringIgnoreCase

--- ch20/src/SortStringIgnoreCase.java
+++ ch20/src/SortStringIgnoreCase.java
@@ -1,5 +1,6 @@
 public class SortStringIgnoreCase {
     public static void main(String[] args) {
-
+        java.util.List<String> cities = java.util.Arrays.asList
+                ("Atlanta", "Savannah", "New York", "Dallas");
     }
 }
