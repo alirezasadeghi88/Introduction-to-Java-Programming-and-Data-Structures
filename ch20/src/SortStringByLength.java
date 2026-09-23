33e171f362fc5beb1790578b2b79ea7fe80c14cc
alirezasadeghi88 <alirezasadeghi0558@gmail.com>
2026-09-23 13:18:33 +0330

    Add class SortStringByLength

--- ch20/src/SortStringByLength.java
+++ ch20/src/SortStringByLength.java
@@ -2,5 +2,10 @@ public class SortStringByLength {
     public static void main(String[] args) {
         String[] cities = {"Atlanta", "Savannah", "New York", "Dallas"};
         java.util.Arrays.sort(cities, new MyComparator());
+
+
+        for (String s : cities) {
+
+        }
     }
 }
