6c3cda42976c46ac9a048bd199e88fce6f3c20a9
alirezasadeghi88 <alirezasadeghi0558@gmail.com>
2026-09-23 13:07:03 +0330

    Add class TestIterator

--- ch20/src/TestIterator.java
+++ ch20/src/TestIterator.java
@@ -5,5 +5,6 @@ public class TestIterator {
     public static void main(String[] args) {
         Collection<String> collection = new ArrayList<>();
         collection.add("New York");
+        collection.add("Atlanta");
     }
 }
