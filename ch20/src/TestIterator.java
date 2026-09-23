6d54a7a398e65bac64cb0cf9a4e7d4497a5ca928
alirezasadeghi88 <alirezasadeghi0558@gmail.com>
2026-09-23 13:06:39 +0330

    Add class TestIterator

--- ch20/src/TestIterator.java
+++ ch20/src/TestIterator.java
@@ -1,5 +1,8 @@
+import java.util.ArrayList;
+import java.util.Collection;
+
 public class TestIterator {
     public static void main(String[] args) {
-
+        Collection<String> collection = new ArrayList<>();
     }
 }
