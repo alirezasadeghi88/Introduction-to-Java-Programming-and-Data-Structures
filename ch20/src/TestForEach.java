8be1eaa8388f6f7038f7381433fb77595e3bdaf7
alirezasadeghi88 <alirezasadeghi0558@gmail.com>
2026-09-23 13:09:20 +0330

    Add class TestForEach

--- ch20/src/TestForEach.java
+++ ch20/src/TestForEach.java
@@ -1,5 +1,8 @@
+import java.util.ArrayList;
+import java.util.Collection;
+
 public class TestForEach {
     public static void main(String[] args) {
-
+        Collection<String> collection = new ArrayList<>();
     }
 }
