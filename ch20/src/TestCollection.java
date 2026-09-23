0ff17c343a27fee9e45ac75d82da1676e4eedf5f
alirezasadeghi88 <alirezasadeghi0558@gmail.com>
2026-09-23 12:49:11 +0330

    Add class TestCollection

--- ch20/src/TestCollection.java
+++ ch20/src/TestCollection.java
@@ -3,5 +3,7 @@ import java.util.ArrayList;
 public class TestCollection {
     public static void main(String[] args) {
         ArrayList<String> collection1 = new ArrayList<>();
+
+        collection1.add("New York");
     }
 }
