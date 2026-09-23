9a89e8aaa4f9e95620811995a92ac69232c9e368
alirezasadeghi88 <alirezasadeghi0558@gmail.com>
2026-09-23 13:19:52 +0330

    Add class SortStringByLength

--- ch20/src/SortStringByLength.java
+++ ch20/src/SortStringByLength.java
@@ -14,7 +14,7 @@ public class SortStringByLength {
             java.util.Comparator<String> {
 
         @Override
-        public int compare(String o1, String o2) {
+        public int compare(String s1, String s2) {
             return s1.length() - s2.length();
         }
     }
