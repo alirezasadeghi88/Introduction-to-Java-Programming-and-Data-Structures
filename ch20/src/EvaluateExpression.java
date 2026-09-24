59468f66e56a1b51daf470654daf9f1eee3dfc94
alirezasadeghi88 <alirezasadeghi0558@gmail.com>
2026-09-24 09:56:11 +0330

    Add class EvaluateExpression

--- ch20/src/EvaluateExpression.java
+++ ch20/src/EvaluateExpression.java
@@ -1,7 +1,8 @@
 public class EvaluateExpression {
     public static void main(String[] args) {
         if (args.length != 1) {
-
+            System.out.println(
+                    "Usage: java EvaluateExpression \"expression\"");
         }
     }
 }
