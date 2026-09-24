597a66cf0b917741a387591b9f352a6ccfdf3983
alirezasadeghi88 <alirezasadeghi0558@gmail.com>
2026-09-24 09:46:02 +0330

    Add class PriorityQueueDemo

--- ch20/src/PriorityQueueDemo.java
+++ ch20/src/PriorityQueueDemo.java
@@ -3,5 +3,6 @@ import java.util.PriorityQueue;
 public class PriorityQueueDemo {
     public static void main(String[] args) {
         PriorityQueue<String> queue1 = new PriorityQueue<>();
+        queue1.offer("Oklahoma");
     }
 }
