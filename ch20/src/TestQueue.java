99b5478656d4d6ec8f15c93e4d6b6cd1d175b4e9
alirezasadeghi88 <alirezasadeghi0558@gmail.com>
2026-09-24 09:43:35 +0330

    Add class TestQueue

--- ch20/src/TestQueue.java
+++ ch20/src/TestQueue.java
@@ -1,5 +1,6 @@
 public class TestQueue {
     public static void main(String[] args) {
         java.util.Queue<String> queue = new java.util.LinkedList<>();
+        queue.offer("Oklahoma");
     }
 }
