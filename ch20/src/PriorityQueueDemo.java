9c2ca88ccf20e5eb986f4adf48111fdbf70acdc3
alirezasadeghi88 <alirezasadeghi0558@gmail.com>
2026-09-24 09:45:48 +0330

    Add class PriorityQueueDemo

--- ch20/src/PriorityQueueDemo.java
+++ ch20/src/PriorityQueueDemo.java
@@ -1,5 +1,7 @@
+import java.util.PriorityQueue;
+
 public class PriorityQueueDemo {
     public static void main(String[] args) {
-
+        PriorityQueue<String> queue1 = new PriorityQueue<>();
     }
 }
