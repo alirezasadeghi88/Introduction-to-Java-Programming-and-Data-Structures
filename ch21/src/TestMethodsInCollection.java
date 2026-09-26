588d53e7efc5e9fa06092d5299e8f43b44a915bf
alirezasadeghi88 <alirezasadeghi0558@gmail.com>
2026-09-26 16:28:36 +0330

    Add class TestMethodsInCollection

--- ch21/src/TestMethodsInCollection.java
+++ ch21/src/TestMethodsInCollection.java
@@ -1,5 +1,6 @@
 public class TestMethodsInCollection {
     public static void main(String[] args) {
         java.util.Set<String> set1 = new java.util.HashSet<>();
+        set1.add("London");
     }
 }
