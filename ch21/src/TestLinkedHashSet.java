8778812b0ce80c22fe5447023117dd7590d44eb3
alirezasadeghi88 <alirezasadeghi0558@gmail.com>
2026-09-26 16:36:21 +0330

    Add class TestLinkedHashSet

--- ch21/src/TestLinkedHashSet.java
+++ ch21/src/TestLinkedHashSet.java
@@ -1,5 +1,8 @@
+import java.util.LinkedHashSet;
+import java.util.Set;
+
 public class TestLinkedHashSet {
     public static void main(String[] args) {
-
+        Set<String> set = new LinkedHashSet<>();
     }
 }
